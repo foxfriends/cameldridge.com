8ae9e33d884f1bf2b1d5b9e51902960751a8beb1
Cameron Eldridge <cameldridge@gmail.com>
2025-04-12 23:13:51 -0400

    theoretical nginx configuration... still not sure how best to manage the certs

---
+++ nginx.tf
@@ -0,0 +1,36 @@
+resource "docker_image" "nginx" {
+  name = "nginx"
+}
+
+resource "docker_container" "nginx" {
+  image   = docker_image.nginx.image_id
+  name    = "cameldridge.com"
+  restart = "unless-stopped"
+
+  ports {
+    internal = 80
+    external = 80
+  }
+
+  ports {
+    internal = 443
+    external = 443
+  }
+
+  volumes {
+    container_path = "/etc/nginx/templates"
+    host_path      = abspath("${path.module}/nginx/templates")
+  }
+
+  volumes {
+    container_path = "/ssl/"
+    host_path      = abspath("${path.module}/nginx/ssl")
+  }
+
+  env = [
+    "COOKIEALYST_PORT=${modules.cookiealyst.port}",
+    "INVENTORY_PORT=${modules.inventory.port}",
+    # HACK: for nginx templating via envsubst, we "escape" the $ as ${DOLLAR}
+    "DOLLAR=$",
+  ]
+}
